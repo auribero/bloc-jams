bab27f4dcc6923356822e3e7f648b997ffcb6e45
Andres Uribe <auriberodriguez@gmail.com>
2017-05-23 23:35:07 -0700

    Assignment 8 transitions: complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,27 @@
 var animatePoints = function() {
   var points = document.getElementsByClassName('point');
 
-  var revealFirstPoint = function(){
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+  function revealPoint(index){
+    index.style.opacity = 1;
+    index.style.transform = "scaleX(1) translateY(0)";
+    index.style.msTransform = "scaleX(1) translateY(0)";
+    index.style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+
+  for(var i=0; i<points.length; i++){
+    revealPoint(points[i]);
+  }
+
+};
+
+var animateHeroTitle = function() {
+  var textNode = document.getElementById('turnUp');
+
+  var revealText = function(){
+    textNode.style.opacity = 1;
+    textNode.style.transform = "scaleX(1) translateY(-50%)";
+    textNode.style.msTransform = "scaleX(1) translateY(-50%)";
+    textNode.style.WebkitTransform = "scaleX(1) translateY(-50%)";
   };
-  var revealSecondPoint = function(){
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  var revealThirdPoint = function(){
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
+  revealText();
 };
