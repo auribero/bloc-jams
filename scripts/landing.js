4e347c3bed6bb786ff854a9dc9b14b5e78c216b7
Andres Uribe <auriberodriguez@gmail.com>
2017-05-24 18:07:36 -0700

    Checkpoint 9 events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,24 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+window.onload = function(){
+
+animateHeroTitle();
+
+  if(window.innerHeight > 950){
+    animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+      animatePoints(pointsArray);
+    }
+  });
+}
+
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 
   function revealPoint(index){
     index.style.opacity = 1;
@@ -7,11 +26,9 @@ var animatePoints = function() {
     index.style.msTransform = "scaleX(1) translateY(0)";
     index.style.WebkitTransform = "scaleX(1) translateY(0)";
   }
-
   for(var i=0; i<points.length; i++){
     revealPoint(points[i]);
   }
-
 };
 
 var animateHeroTitle = function() {
