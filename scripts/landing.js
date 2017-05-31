21b9a841c7bea3f0933b802bf256711a39141504
Andres Uribe <auriberodriguez@gmail.com>
2017-05-30 21:31:43 -0700

    checkpoint 16 jQuery landing: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,44 +1,33 @@
-window.onload = function(){
+$(window).load(function(){
 
-animateHeroTitle();
+  animateHeroTitle();
 
-  if(window.innerHeight > 950){
-    animatePoints(pointsArray);
+  if($(window).height() > 950){
+    animatePoints();
   }
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event) {
-    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-      animatePoints(pointsArray);
+  $(window).scroll(function(event) {
+    if($(window).scrollTop() >= scrollDistance){
+      animatePoints();
     }
   });
-}
+});
 
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-  function revealPoint(index){
-    index.style.opacity = 1;
-    index.style.transform = "scaleX(1) translateY(0)";
-    index.style.msTransform = "scaleX(1) translateY(0)";
-    index.style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-  for(var i=0; i<points.length; i++){
-    revealPoint(points[i]);
-  }
+var animatePoints = function() {
+  var revealPoint = function(){
+    $(this).css({
+      opacity: 1,
+      transform: "scaleX(1) translateY(0)"
+    });
+  };
+  $.each($('.point'), revealPoint);
 };
 
 var animateHeroTitle = function() {
-  var textNode = document.getElementById('turnUp');
-
-  var revealText = function(){
-    textNode.style.opacity = 1;
-    textNode.style.transform = "scaleX(1) translateY(-50%)";
-    textNode.style.msTransform = "scaleX(1) translateY(-50%)";
-    textNode.style.WebkitTransform = "scaleX(1) translateY(-50%)";
-  };
-  revealText();
+  $('.hero-title').css({
+    opacity: 1,
+    transform: "scaleX(1) translateY(-50%)"
+  });
 };
